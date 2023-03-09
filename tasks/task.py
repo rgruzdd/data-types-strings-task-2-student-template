8ec1d3c24f0530fc9790abe22cf278181848f60f
Gruzin <gruzd.roman@inbox.ru>
2023-03-09 18:10:06 +0300

    Commit with a solution

--- tasks/task.py
+++ tasks/task.py
@@ -4,14 +4,11 @@ def get_longest_word( s: str) -> str:
     """
     max_str = ''
     for i in s.split(' '):
-        if len(max_str) < len(i):
-            max_str = i
+        len(max_str) < len(i)
+        max_str = i
 
     return max_str
 
-
-
-
-get_longest_word('Python is simple and effective!')
+print(get_longest_word('Python is simple and effective!'))
 
 
